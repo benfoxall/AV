2f31253560833f784b091d978193946adf893544
Ben Foxall <benfoxall@gmail.com>
2017-12-03 19:32:08 +0000

    Renaming/commenting, etc.

--- js/lib/pixelMap.js
+++ js/lib/pixelMap.js
@@ -1,7 +1,18 @@
-const map = (stride, length) =>
+
+/*
+  This creates a map from pixels to audio samples in a vertical scan way,
+  which can be used to maintain a bit more locality in the source image:
+
+    1 4 7 10 13
+    2 5 8 11 14
+    3 6 9 12 15
+
+  It's probably got a better name (like, transpose) but:
+    1. I'm not totally sure
+    2. I like the word intermingle
+
+*/
+export const intermingle = (stride, vertical) =>
   Array.from({
-    length: stride * length,
-  }, (_, i) => (i % 2) * 10 + Math.floor(i/2))
-
-
-export default map
+    length: stride * vertical,
+  }, (_, i) => (i % vertical) * stride + Math.floor(i / vertical))
--- test/tests.js
+++ test/tests.js
@@ -1,25 +1,32 @@
-import pixelMap from '../js/lib/pixelMap.js'
+import {intermingle} from '../js/lib/pixelMap.js'
 
 const {expect} = chai
 
-describe('pixel map', () => {
+describe('intermingle', () => {
+
   it('is a function', () => {
-    expect(typeof pixelMap).to.equal('function')
+    expect(typeof intermingle).to.equal('function')
   })
 
-
   it('works for a simple case', () => {
-    const result = pixelMap(
+    const result = intermingle(
       10, 2
     )
 
     const expected = [0,10,1,11,2,12,3,13,4,14,5,15,6,16,7,17,8,18,9,19]
 
-    expect(result)
-      .to.eql(
-        expected
-      )
+    expect(result).to.eql(expected)
 
+  })
+
+  it('works for another simple case', () => {
+    const result = intermingle(
+      4, 2
+    )
+
+    const expected = [0,4,1,5,2,6,3,7]
+
+    expect(result).to.eql(expected)
   })
 
 })
